d90f3bb6e9e091e90403e5670b51e6bf75e0778b
Jacob Clark <jacobcs@zoho.com>
2022-10-01 02:20:51 -0700

    feat: full page next

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -10,8 +10,13 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
-      <Box margin="8">
+    <Box
+      margin="0 auto"
+      h="100vh"
+      maxWidth="container.2xl"
+      transition="0.5s ease-out"
+    >
+      <Box>
         <Header />
         <Box as="main" marginY={22}>
           {children}
